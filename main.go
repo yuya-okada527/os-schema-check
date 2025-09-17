02cce5bce5ba35cdd458675f180fcc2c789436c7
yuya-okada527 <yuya.okada527@gmail.com>
2025-09-17 23:30:48 +0900

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "os"
+    "strings"
 )
 
 func main() {
@@ -13,11 +14,18 @@ func main() {
     }
 
     path := args[0]
+    if !strings.HasSuffix(path, ".json") {
+        fmt.Println("Error: file must have .json extension")
+        os.Exit(1)
+    }
+
     if _, err := os.Stat(path); err == nil {
-        fmt.Println("Yes")
+        fmt.Println("File exists")
     } else if os.IsNotExist(err) {
-        fmt.Println("No")
+        fmt.Println("File does not exist")
+        os.Exit(1)
     } else {
         fmt.Printf("Error checking path: %v\n", err)
+        os.Exit(1)
     }
 }
